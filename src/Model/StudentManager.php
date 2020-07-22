e48918d47cf4fc52c6a6125363b148efb16b4168
duong08051997 <duong08051997@gmai.com>
2020-07-22 23:30:52 +0700

    khoi tao class student manager

--- src/Model/StudentManager.php
+++ src/Model/StudentManager.php
@@ -6,5 +6,12 @@ namespace Web\Model;
 
 class StudentManager
 {
+    protected $database;
+    public function __construct()
+    {
+        $db = new DBConnect();
+        $this->database = $db->connect();
+    }
+
 
 }
