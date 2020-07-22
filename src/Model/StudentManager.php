5912b7c10515435338324983c29125f988b9022d
duong08051997 <duong08051997@gmai.com>
2020-07-22 23:32:37 +0700

    tao function getAllStudent

--- src/Model/StudentManager.php
+++ src/Model/StudentManager.php
@@ -13,5 +13,21 @@ class StudentManager
         $this->database = $db->connect();
     }
 
+    public function getAllStudent()
+    {
+        $sql = "SELECT * FROM tbl_student WHERE class_id =:id";
+        $stmt = $this->database->prepare($sql);
+        $stmt->bindParam(":id", $class_id);
+        $stmt->execute();
+        $data = $stmt->fetchAll();
+        $students = [];
+        foreach ($data as $item) {
+            $student = new Student($item['name'], $item['age'], $item['gender'], $item['address'], $item['email'], $item['class_id']);
+            $student->setId($item['id']);
+            array_push($students, $student);
+        }
+        return $students;
+
+    }
 
 }
